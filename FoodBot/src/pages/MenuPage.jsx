b950f6d30c0e1f6b8a791aad01eee1d994110d3b
AARONKOSHY22 <aaronjameskoshy@gmail.com>
2024-03-15 23:25:41 +0530

    MenuUpdate

--- FoodBot/src/pages/MenuPage.jsx
+++ FoodBot/src/pages/MenuPage.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import { Outlet } from 'react-router-dom'; // Import Outlet
 
 const MenuPage = () => {
-  return <div>Menu Page Content</div>;
+  // Use Outlet to render child routes
+  return (
+    <div>
+      <h2>Menu Page Content</h2>
+      <Outlet /> {/* Child routes (View, Add, Edit) are rendered here */}
+    </div>
+  );
 };
 
 export default MenuPage;
